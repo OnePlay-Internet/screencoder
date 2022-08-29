5bc25972f242c0abac7be542c54a41382c337cc8
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-08-29 14:28:25 +0700

    update example

---
+++ examples/go_webrtc/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	proxy "github.com/OnePlay-Internet/webrtc-proxy"
+	"github.com/OnePlay-Internet/webrtc-proxy/listener"
+	"github.com/OnePlay-Internet/webrtc-proxy/util/config"
+	"github.com/pion/webrtc/v3"
+)
+
+func main() {
+	var token string
+	args := os.Args[1:]
+	for i, arg := range args {
+		if arg == "--token" {
+			token = args[i+1]
+		} else if arg == "--help" {
+			fmt.Printf("--token |  server token\n")
+			return
+		}
+	}
+
+	if token == "" {
+		return
+	}
+
+	grpc := config.GrpcConfig{
+		Port:          30000,
+		ServerAddress: "grpc.signaling.thinkmay.net",
+		Token:         token,
+	}
+	rtc := config.WebRTCConfig{
+		Ices: []webrtc.ICEServer{{
+			URLs: []string{
+				"stun:stun.l.google.com:19302",
+			},
+		}, {
+			URLs: []string{
+				"stun:workstation.thinkmay.net:3478",
+			},
+		},
+		},
+	}
+	br := []*config.BroadcasterConfig{}
+	lis := []*config.ListenerConfig{{
+		Source: "screencoder",
+
+		DataType: "sample",
+
+		MediaType: "video",
+		Name:      "gpuGstreamer",
+		Codec:     webrtc.MimeTypeH264,
+	}}
+
+	for {
+		chans := config.DataChannelConfig{
+			Offer: true,
+			Confs: map[string]*struct {
+				Send    chan string
+				Recv    chan string
+				Channel *webrtc.DataChannel
+			}{
+				"hid": {
+					Send:    make(chan string),
+					Recv:    make(chan string),
+					Channel: nil,
+				},
+			},
+		}
+
+		go func() {
+			for {
+				channel := chans.Confs["hid"]
+				if channel != nil {
+					str := <-chans.Confs["hid"].Recv
+					go ParseHIDInput(str)
+				} else {
+					return
+				}
+			}
+		}()
+
+		Lists := make([]listener.Listener, 0)
+		for _, lis_conf := range lis {
+			var Lis listener.Listener
+			if lis_conf.Source == "screencoder" {
+				Lis = NewScreencoderListener(*lis_conf)
+			} else {
+				fmt.Printf("Unimplemented listener\n")
+				continue
+			}
+			
+
+			Lists = append(Lists, Lis)
+		}
+
+		prox, err := proxy.InitWebRTCProxy(nil, &grpc, &rtc, br, &chans, Lists)
+		if err != nil {
+			fmt.Printf("failed to init webrtc proxy, try again in 2 second\n")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		<-prox.Shutdown
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,41 @@ module github.com/Oneplay-Internet/screencoder
 go 1.18
 
 require (
-	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtp v1.7.13 // indirect
+	github.com/OnePlay-Internet/webrtc-proxy v1.0.3
+	github.com/pion/randutil v0.1.0
+	github.com/pion/rtp v1.7.13
+	github.com/pion/webrtc/v3 v3.1.43
+)
+
+require (
+	github.com/pions/dtls v1.0.2 // indirect
+	github.com/pions/pkg v0.0.0-20181115215726-b60cd756f712 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pion/datachannel v1.5.2 // indirect
+	github.com/pion/dtls/v2 v2.1.5 // indirect
+	github.com/pion/ice/v2 v2.2.6 // indirect
+	github.com/pion/interceptor v0.1.11 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.5 // indirect
+	github.com/pion/rtcp v1.2.9 // indirect
+	github.com/pion/sctp v1.8.2 // indirect
+	github.com/pion/sdp/v3 v3.0.5 // indirect
+	github.com/pion/srtp/v2 v2.0.10 // indirect
+	github.com/pion/stun v0.3.5 // indirect
+	github.com/pion/transport v0.13.1 // indirect
+	github.com/pion/turn/v2 v2.0.8 // indirect
+	github.com/pion/udp v0.1.1 // indirect
+	github.com/pions/webrtc v1.2.0
+	golang.org/x/crypto v0.0.0-20220516162934-403b01795ae8 // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
+	golang.org/x/sys v0.0.0-20220622161953-175b2fd9d664 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
